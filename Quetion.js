8dbe01761b6ff00daa94efe06fdcccd410b5b9f7
XxCoolCrafterxX <86944981+XxCoolCrafterxX@users.noreply.github.com>
2021-08-12 14:06:47 +0530

    Add files via upload

--- Quetion.js
+++ Quetion.js
@@ -1,8 +1,14 @@
-<script type="text/javascript">
-$(document).ready(function(){
-    $('.faqlink').click(function(){
-        $('.content').hide();
-        $(this).next('.content').show();
-    });
-});
-</script>
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
+
+    }))
+}) 
